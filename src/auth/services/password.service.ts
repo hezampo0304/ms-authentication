a1659c78ba9eeb7a4101ace395488d921158f77b
Henry Zambrano Polentino <hzambra@bancolombia.com.co>
2026-07-29 17:37:57 -0500

    feat: implement login functionality with JWT authentication and user session management

---
+++ src/auth/services/password.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class PasswordService {
+
+  async hash(password: string): Promise<string> {
+    return bcrypt.hash(password, 12);
+  }
+
+  async compare(
+    password: string,
+    hash: string,
+  ): Promise<boolean> {
+    return bcrypt.compare(password, hash);
+  }
+}
